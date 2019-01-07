b859a9fabe03f6c98e23665f8aa15c49d5e94742
ravihd <ec2-user@ip-172-31-70-156.ec2.internal>
2019-01-07 09:53:46 +0000

    updated DB files

--- db/migrate/20181210103540_create_books.rb
+++
@@ -1,8 +0,0 @@
-class CreateBooks < ActiveRecord::Migration[5.2]
-  def change
-    create_table :books do |t|
-      t.string :bookname
-      t.string :author
-    end
-  end
-end
--- db/migrate/20181210103745_add_price_to_books.rb
+++
@@ -1,7 +0,0 @@
-class AddPriceToBooks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :books, :price, :intiger
-      add_column :books, :created_at, :datetime
-    add_column :books, :updtaed_at, :datetime
-  end
-end
